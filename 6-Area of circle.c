95f5a74b7fdcaacf00696e28f47a2fadce6dc8a4
Atul Sharma <atulsharma20may@gmail.com>
2020-02-08 11:23:25 +0530

    To upload files only

---
+++ 6-Area of circle.c
@@ -0,0 +1,11 @@
+//To calculate area of circle
+#include<stdio.h>
+
+void main(){
+    float radius;
+    
+    printf("Enter the value radius of circle\n");
+    scanf("%f", &radius);
+    
+    printf("the area of radius is %f m^2.", 3.14*radius*radius);        
+}
