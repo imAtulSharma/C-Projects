aacc13cca702b59eb7b79fff0a9cf3e95bcae185
Atul Sharma <atulsharma20may@gmail.com>
2020-02-15 11:19:10 +0530

    To upload files only

---
+++ 2-Adding two numbers.c
@@ -0,0 +1,11 @@
+// Two print the addition of two numbers
+#include<stdio.h>
+
+void main(){
+    int number1, number2;
+
+    printf("Enter the value of two numbers\n");
+    scanf("%d %d", &number1, &number2);
+
+    printf("The sum of numbers is %d", number1 + number2);
+}
