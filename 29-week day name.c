07133e10071dfba8c7eb29bb0df4dfc0da21472b
Atul Sharma <atulsharma20may@gmail.com>
2020-02-17 11:29:32 +0530

    To upload files only

---
+++ 29-week day name.c
@@ -0,0 +1,36 @@
+//Week day name using Switch Case
+#include<stdio.h>
+
+void main()
+{    
+     int index;
+     
+     printf("Enter Weekday number to know its name\n");
+     scanf("%d", &index);
+     
+     switch(index)
+     {	  
+        case 1: printf("It is Monday");
+             break;
+
+        case 2: printf("It is Teusday");
+             break;
+
+        case 3: printf("It is Wednesday");
+             break;
+
+        case 4: printf("It is Thursday");
+             break;
+
+        case 5: printf("It is Friday");
+             break;
+
+        case 6: printf("It is Saturday");
+             break;
+
+        case 7: printf("It is Sunday");
+             break;
+
+        default: printf("Not a valid number.");
+     }
+}
