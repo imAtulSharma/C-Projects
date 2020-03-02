2fe66473823dd6feb1c4b22499dea8846f2db386
Atul Sharma <atulsharma20may@gmail.com>
2020-03-02 15:56:48 +0530

    To upload files only

---
+++ 59-Using loops for array input.c
@@ -0,0 +1,18 @@
+//Programme to use loops for array input and printing array
+#include<stdio.h>
+
+void main(){
+    int array[5];
+
+    for(int i = 0; i < 5; i++){
+        printf("Enter the %d element of array\n", i+1);
+        scanf("%d", &array[i]);
+    }
+
+    printf("Your array is...\n");
+
+    for (int i = 0; i < 5; i++)
+    {
+        printf("The %d element of array is %d\n", i+1, array[i]);
+    }
+}
