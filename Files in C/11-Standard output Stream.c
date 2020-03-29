93970a29201cb676f00b86bb3cadb77aaf465a7d
Atul Sharma <atulsharma20may@gmail.com>
2020-03-29 10:09:31 +0530

    To uplaod files

---
+++ Files in C/11-Standard output Stream.c
@@ -0,0 +1,10 @@
+//Programme to print through the standard output stream
+
+#include <stdio.h>
+
+void main(){
+    
+    char data[] = "My name is Atul sharma";
+    
+    fprintf(stdout, "%s", data); //Print in the stdout stream
+}
